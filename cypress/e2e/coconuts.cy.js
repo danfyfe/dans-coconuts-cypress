d99c4acc0b27217adb76d384aa8a6ef94059b758
Dan Fyfe <fyfedaniel@gmail.com>
2024-03-28 16:38:35 -0400

    remove workflow, add empty tests

---
+++ cypress/e2e/coconuts.cy.js
@@ -0,0 +1,13 @@
+describe("Dan's Coconuts Live - Coconut Page", () => {
+  it('loads when visited', () => {
+    cy.visit('https://www.danscoconuts.com/coconuts')
+  });
+
+  it('renders the add coconut button', () => {
+
+  });
+
+  it('adds a coconut when the add coconut button is clicked', () => {
+
+  });
+})
