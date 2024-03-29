4c8f63a7beb5b93dd94dde6636c6672c44126565
Dan Fyfe <fyfedaniel@gmail.com>
2024-03-29 10:17:58 -0400

    more config changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,9 @@ module.exports = {
   projectId: "3u3goe",
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000',
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 };
