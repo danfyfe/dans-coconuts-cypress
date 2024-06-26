d67af357b3e330151cc8aaf73dec7866ba957ad4
Dan Fyfe <fyfedaniel@gmail.com>
2024-06-26 16:13:36 -0400

    update weather tests

--- cypress/e2e/weather.cy.ts
+++ cypress/e2e/weather.cy.ts
@@ -1,33 +1,36 @@
 describe("Dan's Coconuts - Weather", () => {
   it('weather button toggles the weather when clicked', () => {
     cy.visit('/')
-      .get('#weather-widget-btn')
-      .click()
-      .get('#weather-btn')
-      .click()
-      .get('#rain')
-  });
-
-  it('weather carries over on route change', () => {
-    cy.visit('/')
+    .get('#home-close-modal').click() // close tour welcome modal
     .get('#weather-widget-btn')
     .click()
     .get('#weather-btn')
     .click()
     .get('#rain')
-    .visit('/coconuts')
+  });
+
+  it('weather carries over on route change', () => {
+    cy.visit('/')
+    .get('#home-close-modal').click() // close tour welcome modal
+    .get('#weather-widget-btn')
+    .click()
+    .get('#weather-btn')
+    .click()
+    .get('#rain')
+    .visit('/github')
     .get('#rain')
   });
 
   it('remembers the weather state on different visits or refresh', () => {
     cy.visit('/')
+    .get('#home-close-modal').click() // close tour welcome modal
     .get('#weather-widget-btn')
     .click()
     .get('#weather-btn')
     .click()
     .get('#rain')
-    .visit('https://www.danscoconuts.com')
     .visit('/')
+    .get('#home-close-modal').click() // close tour welcome modal
     .get('#rain')
   });
 });
