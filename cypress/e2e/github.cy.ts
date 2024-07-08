86ba84ca093ab43eff72dde9b4762691fde51538
Dan Fyfe <fyfedaniel@gmail.com>
2024-07-08 09:34:01 -0400

    update github page tests

--- cypress/e2e/github.cy.ts
+++ cypress/e2e/github.cy.ts
@@ -12,7 +12,7 @@ describe("Dan's Coconuts - Github", () => {
     .get('@container')
     .get('p').then(($p) => {
       expect($p).to.have.length(4)
-      expect($p.eq(0)).to.contain('Check out the repositories for this project')
+      expect($p.eq(0)).to.contain('Check out my pinned repositories')
       expect($p.eq(1)).to.contain('Click here for the main profile page')
       expect($p.eq(2)).to.contain('Unfortunately, most past professional work experience and projects cannot be shared here, but I am happy to do a walkthrough. Reach out through the contact page.')
       expect($p.eq(3)).to.contain('*This page is built using the GitHub GraphQL endpoint')
@@ -25,13 +25,15 @@ describe("Dan's Coconuts - Github", () => {
     const githubBaseUrl = "https://github.com/danfyfe";
     cy.visit('/github')
     .get('#github-repo-links a').then(($links) => {
-      expect($links).to.have.length(2)
+      expect($links).to.have.length(4)
       expect($links.eq(0)).to.contain('dans-coconuts-fe')
       expect($links.eq(0)).to.have.attr('href', `${githubBaseUrl}/dans-coconuts-fe`)
-      // expect($links.eq(1)).to.contain('dans-coconuts-be')
-      // expect($links.eq(1)).to.have.attr('href', `${githubBaseUrl}/dans-coconuts-be`)
       expect($links.eq(1)).to.contain('dans-coconuts-cypress')
       expect($links.eq(1)).to.have.attr('href', `${githubBaseUrl}/dans-coconuts-cypress`)
+      expect($links.eq(2)).to.contain('dans-coconuts-nightwatch')
+      expect($links.eq(2)).to.have.attr('href', `${githubBaseUrl}/dans-coconuts-nightwatch`)
+      expect($links.eq(3)).to.contain('web-dev-components')
+      expect($links.eq(3)).to.have.attr('href', `${githubBaseUrl}/web-dev-components`)
     })
   });
 });
